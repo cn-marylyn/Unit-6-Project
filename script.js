e6b3caa74d0d02f0084308641bd5646c997ff3fa
marylyn-rogel <86623277+marylyn-rogel@users.noreply.github.com>
2021-07-07 16:20:24 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,37 @@
+$("img").hide();
+
+$(".stay").click(function() {
+    $(".movie").slideDown();
+    $(".bed").slideDown();
+    $(".pick").text("Yay to staying in! Now, would you rather watch a movie or go to bed? *Click the image*");
+    $(".concert").hide();
+    $(".stay").hide();
+    $("body").css("background-color", "yellow");
+
+});
+$(".concert").click(function() {
+    $(".kehlani").show();
+    $(".jessie").show();
+    $(".pick").text("Yay to concerts! Which concert would you like to attend? Kehlani or Jessie Reyez? *Click the image*");
+    $(".stay").hide();
+    $(".concert").hide();
+});
+$(".kehlani").click(function() {
+    $(".jessie").hide();
+    $(".pick").text("Enjoy the concert!");
+});
+
+$(".jessie").click(function() {
+    $(".kehlani").hide();
+    $(".pick").text("Enjoy the concert!");
+});
+
+
+$(".movie").click(function() {
+    $(".bed").hide();
+    $(".pick").text("Have fun watching the movie and staying in!");
+});
+$(".bed").click(function() {
+    $(".movie").hide();
+    $(".pick").text("Have a good night!");
+});
