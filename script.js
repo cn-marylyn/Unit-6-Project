cccc53dd3d07c6e0d527ef3eb1fee12b929f59bb
marylyn-rogel <86623277+marylyn-rogel@users.noreply.github.com>
2021-07-07 17:33:05 -0500

    Created using Popcode: https://popcode.org

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 $("img").hide();
+$(".reset").hide();
 
 $(".stay").click(function() {
     $(".movie").slideDown();
@@ -7,6 +8,9 @@ $(".stay").click(function() {
     $(".concert").hide();
     $(".stay").hide();
     $("body").css("background-color", "orange");
+    $(".reset").show();
+
+
 
 });
 $(".concert").click(function() {
@@ -15,24 +19,43 @@ $(".concert").click(function() {
     $(".pick").text("Yay to concerts! Which concert would you like to attend? Kehlani or Jessie Reyez? *Click the image*");
     $(".stay").hide();
     $(".concert").hide();
-        $("body").css("background-color", "lightpink");
+    $("body").css("background-color", "lightpink");
+    $(".reset").show();
+
 });
 $(".kehlani").click(function() {
     $(".jessie").hide();
     $(".pick").text("Enjoy the concert!");
+    $(".reset").show();
+
+
 });
 
 $(".jessie").click(function() {
     $(".kehlani").hide();
     $(".pick").text("Enjoy the concert!");
+    $(".reset").show();
 });
 
 
 $(".movie").click(function() {
     $(".bed").hide();
     $(".pick").text("Have fun watching the movie and staying in!");
+    $(".reset").show();
 });
+
 $(".bed").click(function() {
     $(".movie").hide();
     $(".pick").text("Have a good night!");
+    $(".reset").show();
+});
+
+$(".reset").click(function() {
+    $(".stay").slideDown();
+    $(".concert").slideDown();
+    $("img").hide();
+    $(".reset").hide();
+    $("body").css("background-color", "lightblue");
+
+
 });
